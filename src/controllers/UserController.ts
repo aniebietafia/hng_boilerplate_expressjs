3588c80f67dbd6ef824a38a0ecaaee5c6cecc7fe
aniebietafia <aniebietafia87@gmail.com>
2024-08-04 00:20:44 +0100

    feat: update user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -95,6 +95,158 @@ class UserController {
     });
   });
 
+  /**
+   * @swagger
+   * /api/v1/user/{id}:
+   *  put:
+   *   tags:
+   *     - User
+   *   summary: Update User Profile
+   *   description: Update the profile of a user
+   *   security:
+   *     - bearerAuth: []
+   *   parameters:
+   *     - in: path
+   *       name: id
+   *       required: true
+   *       schema:
+   *         type: string
+   *         description: The ID of the user
+   *   requestBody:
+   *     required: true
+   *     content:
+   *       multipart/form-data:
+   *         schema:
+   *           type: object
+   *           properties:
+   *             profile_pic_url:
+   *               type: string
+   *               format: binary
+   *               description: The user's profile picture
+   *       application/json:
+   *         schema:
+   *           type: object
+   *           properties:
+   *             first_name:
+   *               type: string
+   *               example: John
+   *             last_name:
+   *               type: string
+   *               example: Doe
+   *             phone:
+   *               type: string
+   *               example: 08012345678
+   *             username:
+   *               type: string
+   *               example: johndoe
+   *             jobTitle:
+   *              type: string
+   *              example: Software Engineer
+   *             pronouns:
+   *              type: string
+   *              example: He/Him
+   *             social_links:
+   *              type: object
+   *             properties:
+   *              twitter:
+   *               type: string
+   *               example: https://twitter.com/johndoe
+   *             linkedin:
+   *               type: string
+   *               example: https://linkedin.com/johndoe
+   *   responses:
+   *     200:
+   *       description: User profile updated successfully
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               id:
+   *                 type: string
+   *                 example: 315e0834-e96d-4ddc-9974-726e8c1e9cf9
+   *               name:
+   *                 type: string
+   *                 example: John Doe
+   *               email:
+   *                 type: string
+   *                 example: johndoe@test.com
+   *               google_id:
+   *                 type: string
+   *                 example: null
+   *               isVerified:
+   *                 type: boolean
+   *                 example: true
+   *               role:
+   *                 type: string
+   *                 example: user
+   *               profile:
+   *                 type: object
+   *                 properties:
+   *                   id:
+   *                     type: string
+   *                     example: 315e0834-e96d-4ddc-9974-726e8c1e9cf9
+   *                   username:
+   *                     type: string
+   *                     example: johndoe
+   *                   jobTitle:
+   *                     type: string
+   *                     example: Software Engineer
+   *                   language:
+   *                     type: string
+   *                     example: English
+   *                   profile_pic_url:
+   *                     type: string
+   *                     example: https://avatar.com/avatar.png
+   *   400:
+   *     description: Bad request
+   *     content:
+   *       application/json:
+   *         schema:
+   *           type: object
+   *           properties:
+   *             status:
+   *               type: string
+   *               example: unsuccessful
+   *             status_code:
+   *               type: integer
+   *               example: 400
+   *             message:
+   *               type: string
+   *               example: Valid id must be provided
+   *   404:
+   *     description: Not found
+   *     content:
+   *       application/json:
+   *         schema:
+   *           type: object
+   *           properties:
+   *             status:
+   *               type: string
+   *               example: unsuccessful
+   *             status_code:
+   *               type: integer
+   *               example: 404
+   *             message:
+   *               type: string
+   *               example: User not found
+   *   500:
+   *     description: Server Error
+   *     content:
+   *       application/json:
+   *         schema:
+   *           type: object
+   *           properties:
+   *             status:
+   *               type: string
+   *               example: unsuccessful
+   *             status_code:
+   *               type: integer
+   *               example: 500
+   *             message:
+   *               type: string
+   *               example: Internal Server Error
+   */
   static updateUser = asyncHandler(async (req: Request, res: Response) => {
     const user = await userService.updateUserProfile(req.params.id, req.body, req.file);
     sendJsonResponse(res, 200, "Profile successfully updated", user);
